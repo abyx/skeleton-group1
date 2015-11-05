4a86c9bc6beaf20acc5692ebf2bf08be45256efc
s128122014 <s1.28.12.2014@gmail.com>
2015-11-05 11:06:19 +0200

    hh

--- app.js
+++ app.js
@@ -419,16 +419,4 @@ function SavePost(list)
   return res;
 }
 
-app.post('/getUser', function(request, response) {
-
-  var user = "undefined";
-   
-  if (request.body != "undefined") 
-  {
-  	console.log("request body is " , request.body);
-  	var user = {userName : request.body.userName, userId: request.body.userId, userRank = 0}
-  }
-  
-  user = listOfRidesData.getUser(user);
-  response.send(user);
-});
+ 
